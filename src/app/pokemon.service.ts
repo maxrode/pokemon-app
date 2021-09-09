bac6cd43592b67a650dd3fef181de8cd6a301c41
maxrode <mattia.rosin@123A>
2021-09-09 11:23:04 +0200

    aggiunta funzione per resituire il risultato del api

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+export interface PokemonApiResult {
+  count : number
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { 
+
+  }
+
+  pokemonApi() : Observable<PokemonApiResult>{
+    return this.httpClient.get<PokemonApiResult>("https://pokeapi.co/api/v2/pokemon")
+  }
 }
